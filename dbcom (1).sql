e5b9d6497f8461b593dfb44bca61be39eca20fbc
John Nathaniel <118042213+Jirenn77@users.noreply.github.com>
2025-03-07 17:09:43 +0800

    Add files via upload

---
+++ dbcom (1).sql
@@ -0,0 +1,549 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2025 at 09:55 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`, `role`) VALUES
+(1, 'admin@gmail.com', 'password123', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `status` enum('Active','Inactive') DEFAULT 'Active',
+  `service_link` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`, `status`, `service_link`, `created_at`) VALUES
+(1, 'Hair Products', 'Products related to hair care.', 'Active', 'https://example.com/hair-products', '2025-03-05 05:20:04'),
+(2, 'Body and Relaxing Products', 'Products for body care and relaxation.', 'Active', 'https://example.com/body-products', '2025-03-05 05:20:04'),
+(3, 'Diode Laser Products', 'Products for diode laser treatments.', 'Active', 'https://example.com/laser-products', '2025-03-05 05:20:04'),
+(4, 'Nails and Foot Services', 'Services for nails and foot care.', 'Active', 'https://example.com/nails-services', '2025-03-05 05:20:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomerID` int(11) NOT NULL,
+  `CustomerName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `ContactDetails` varchar(255) NOT NULL,
+  `Balance` decimal(10,2) DEFAULT 0.00,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`CustomerID`, `CustomerName`, `email`, `ContactDetails`, `Balance`, `user_id`) VALUES
+(1, 'Lucy Anne', 'lucy@gmail.com', '0952421568', -2900.00, 1),
+(2, 'Annie', 'annie@gmail.com', '09554798897', 0.00, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `quantity_in_hand` int(11) NOT NULL,
+  `quantity_to_be_received` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `InvoiceID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `TotalAmount` decimal(10,2) NOT NULL,
+  `InvoiceDate` datetime NOT NULL,
+  `DueDate` datetime NOT NULL,
+  `PaymentStatus` enum('Paid','Unpaid') NOT NULL DEFAULT 'Unpaid',
+  `PaidAmount` decimal(10,2) NOT NULL DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`InvoiceID`, `CustomerID`, `ProductID`, `Quantity`, `TotalAmount`, `InvoiceDate`, `DueDate`, `PaymentStatus`, `PaidAmount`) VALUES
+(1, 2, 6, 1, 2599.00, '2024-10-18 21:18:47', '2024-11-17 21:18:47', 'Unpaid', 0.00),
+(2, 1, 4, 1, 9290.00, '2024-10-18 21:22:43', '2024-11-17 21:22:43', 'Paid', 0.00),
+(3, 1, 10, 1, 16840.00, '2024-10-18 22:24:50', '2024-11-17 22:24:50', 'Paid', 0.00),
+(4, 1, 15, 1, 1500.00, '2024-10-18 22:38:38', '2024-11-20 00:00:00', 'Paid', 0.00),
+(5, 1, 12, 2, 5000.00, '2024-10-19 18:00:13', '2024-10-12 00:00:00', 'Paid', 0.00),
+(6, 2, 7, 2, 1798.00, '2024-10-19 18:48:52', '2024-10-31 00:00:00', 'Paid', 0.00),
+(7, 1, 8, 1, 19795.00, '2024-10-21 16:07:25', '2024-10-30 00:00:00', 'Paid', 0.00),
+(10, 1, 5, 1, 2900.00, '2024-10-21 16:26:40', '2024-10-31 00:00:00', 'Paid', 0.00),
+(13, 1, 11, 1, 27990.00, '2024-10-21 16:35:50', '2024-10-31 00:00:00', 'Unpaid', 1190.00),
+(15, 2, 12, 1, 2500.00, '2024-10-24 09:12:22', '2024-11-14 00:00:00', 'Unpaid', 0.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `stockQty` int(11) NOT NULL,
+  `service` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `unitPrice` decimal(10,2) NOT NULL,
+  `supplier` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `category`, `type`, `stockQty`, `service`, `description`, `unitPrice`, `supplier`) VALUES
+(3, 'Loreal Paris Elvive', 'Hair Product', 'Hair Services', 45, 'Hair Treatment', 'Professional hair care product', 395.00, 'James'),
+(4, 'GiGi Honee Wax', 'Skincare Product', 'Underarm Services', 56, 'Waxing Service', 'Professional waxing product', 18.50, 'SkinCare Solutions'),
+(5, 'Kerazon Brazilian Hair', 'Hair Product', 'Hair Services', 56, 'Hair Extension', 'High-quality Brazilian hair', 120.00, 'Hair World'),
+(6, 'Majestic Hair Botox', 'Hair Product', 'Hair Services', 56, 'Hair Treatment', 'Professional hair botox treatment', 85.00, 'Luxury Hair Care'),
+(14, 'Head & Shoulders', 'Hair Product', 'Hair Services', 4, '', '', 4.00, 'Unilab');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `PaymentID` int(11) NOT NULL,
+  `InvoiceID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `Amount` decimal(10,2) NOT NULL,
+  `PaymentDate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`PaymentID`, `InvoiceID`, `CustomerID`, `Amount`, `PaymentDate`) VALUES
+(1, 3, 1, 840.00, '2024-10-21 12:48:45'),
+(2, 7, 1, 19795.00, '2024-10-21 21:26:27'),
+(16, 13, 1, 990.00, '2024-10-22 11:38:04'),
+(17, 13, 1, 100.00, '2024-10-22 12:21:43'),
+(18, 13, 1, 100.00, '2024-10-24 00:12:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `price_list`
+--
+
+CREATE TABLE `price_list` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `details` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `price_list`
+--
+
+INSERT INTO `price_list` (`id`, `name`, `details`, `description`) VALUES
+(1, 'Membership Discount', '20% Markdown', 'For loyal customers'),
+(2, 'Holiday Discount', '10% Markdown', 'Seasonal discounts');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(255) NOT NULL,
+  `Price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `Price`) VALUES
+(1, 'Gaming Laptop', 50499.00),
+(2, 'Mechanical Keyboard', 1500.00),
+(3, 'Gaming Mouse', 1000.00),
+(4, '27-inch Monitor', 9290.00),
+(5, 'External Hard Drive 1TB', 2900.00),
+(6, 'Wireless Headset', 2599.00),
+(7, 'USB-C Docking Station', 899.00),
+(8, 'Graphics Card NVIDIA RTX 3060', 19795.00),
+(9, '32GB RAM DDR4', 15094.00),
+(10, 'Intel i7 Processor', 16840.00),
+(11, 'Gaming Chair', 27990.00),
+(12, 'Webcam 1080p', 2500.00),
+(13, 'Microphone USB', 1500.00),
+(14, 'Office Desk', 5000.00),
+(15, 'Laptop Backpack', 1500.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_activity`
+--
+
+CREATE TABLE `sales_activity` (
+  `id` int(11) NOT NULL,
+  `type` enum('to_be_packed','to_be_shipped','to_be_delivered','to_be_invoiced') DEFAULT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`id`, `name`, `link`) VALUES
+(1, 'Hair Services', 'https://example.com/hair-services'),
+(2, 'Body Services', 'https://example.com/body-services'),
+(3, 'Laser Services', 'https://example.com/laser-services'),
+(4, 'Nail Services', 'https://example.com/nail-services');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `TransactionID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `TransactionType` enum('Credit','Debit') NOT NULL,
+  `Amount` decimal(10,2) NOT NULL,
+  `TransactionDate` datetime NOT NULL DEFAULT current_timestamp(),
+  `Description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`TransactionID`, `CustomerID`, `ProductID`, `TransactionType`, `Amount`, `TransactionDate`, `Description`) VALUES
+(1, 2, 6, 'Debit', 2599.00, '2024-10-16 22:03:11', 'Paid'),
+(2, 2, 3, 'Credit', 1000.00, '2024-10-17 21:01:45', 'Owe'),
+(5, 1, 10, 'Credit', 16840.00, '2024-10-18 22:24:50', 'Owe'),
+(6, 1, 15, 'Credit', 1500.00, '2024-10-18 22:38:38', 'Owe'),
+(7, 1, 12, 'Debit', 5000.00, '2024-10-19 18:00:13', 'Cash'),
+(8, 2, 7, 'Debit', 1798.00, '2024-10-19 18:48:52', 'Cash'),
+(9, 1, 8, 'Credit', 19795.00, '2024-10-21 16:07:25', 'Owe'),
+(14, 1, 5, 'Credit', 2900.00, '2024-10-21 16:32:17', 'Owe'),
+(17, 2, 12, 'Credit', 2500.00, '2024-10-24 09:12:22', 'Owe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('staff','customer') NOT NULL DEFAULT 'customer',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `CustomerID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`, `role`, `created_at`, `CustomerID`) VALUES
+(1, 'user@gmail.com', 'password123', 'customer', '2024-10-18 18:12:01', 1),
+(3, 'user2@gmail.com', 'user2', 'customer', '2024-10-22 12:46:06', 2),
+(5, 'wanna@gmail.com', '$2y$10$swl/yKsANuRIbPu3J22HV.ay6NLGxafAJjP.upDgClNPU68ru22E2', 'customer', '2025-03-06 04:29:09', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerID`),
+  ADD KEY `fk_user` (`user_id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`InvoiceID`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`PaymentID`),
+  ADD KEY `InvoiceID` (`InvoiceID`),
+  ADD KEY `payments_ibfk_2` (`CustomerID`);
+
+--
+-- Indexes for table `price_list`
+--
+ALTER TABLE `price_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Indexes for table `sales_activity`
+--
+ALTER TABLE `sales_activity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`TransactionID`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `fk_customer_id` (`CustomerID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `InvoiceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `PaymentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `price_list`
+--
+ALTER TABLE `price_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `sales_activity`
+--
+ALTER TABLE `sales_activity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`),
+  ADD CONSTRAINT `invoices_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`InvoiceID`) REFERENCES `invoices` (`InvoiceID`),
+  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`),
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_customer_id` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
